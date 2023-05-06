1a124b1ffcbc1844ec695560b97898ad229ab586
viniOnRails <57204434+lanzgit@users.noreply.github.com>
2023-05-06 19:22:25 -0300

    Initial Commit

---
+++ DiagramAI/DiagramAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiagramAIApp.swift
+//  DiagramAI
+//
+//  Created by Vinicius Vianna on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DiagramAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
